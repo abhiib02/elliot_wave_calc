00d34ee8ff08506c0856fa7da9d8c0fa0786c239
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-01-31 11:30:36 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -301,7 +301,7 @@ function show() {
       <td>${coinarray[i]}</td>
       <td>${arr[0]}</td>
       <td>${arr[1]}</td>
-      <td><button class="btn btn-danger" onclick="delCoin(${coinarray[i]})" ><i class="fas fa-times-circle"></i></button></td>
+      <td><button class="btn btn-danger" onclick="delCoin(${coinarray[i].toString()})" ><i class="fas fa-times-circle"></i></button></td>
       
       
     </tr>`;
