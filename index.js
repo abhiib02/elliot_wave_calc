9aa64fac5a0b7136ba4a90799817533b2312a507
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-09-21 19:57:45 +0530

    debugging

--- index.js
+++ index.js
@@ -345,7 +345,7 @@ function show() {
 
 
 function delCoin(coin) {
-  console.log(JSON.stringify(coin));
+  console.log(Object.values(coin));
   /*if (confirm(`Are you sure you want to delete  ${coin} ?`)) {
     var coinarray = JSON.parse(localStorage.getItem('coins_arr'));
     console.log(coinarray)
